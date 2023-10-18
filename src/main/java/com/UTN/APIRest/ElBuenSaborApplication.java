6a437d3676b3ac3d9f07fb359fb460d4600e9d9a
franSaavedra010011 <franjsaavedra@gmail.com>
2023-10-18 15:07:15 -0300

    Es la estructura base faltan algunas clases

---
+++ src/main/java/com/UTN/APIRest/ElBuenSaborApplication.java
@@ -0,0 +1,13 @@
+package com.UTN.APIRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElBuenSaborApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElBuenSaborApplication.class, args);
+	}
+
+}
